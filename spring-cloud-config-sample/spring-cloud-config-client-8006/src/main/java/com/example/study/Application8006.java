7e087d4ef8b2542975e261b74ab1fd5646f5c830
alprogor <pwxgo1996@163.com>
2020-05-16 21:15:30 +0800

    使用Spring Cloud Bus来更新Spring Cloud Config的配置文件

---
+++ spring-cloud-config-sample/spring-cloud-config-client-8006/src/main/java/com/example/study/Application8006.java
@@ -0,0 +1,11 @@
+package com.example.study;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application8006.class,args);
+    }
+}
