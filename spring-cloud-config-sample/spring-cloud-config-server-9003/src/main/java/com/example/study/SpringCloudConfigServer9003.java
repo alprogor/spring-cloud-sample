a5a11e831820e11170546d188f80c5b5ccc21345
alprogor <pwxgo1996@163.com>
2020-05-16 20:45:30 +0800

    给Spring Cloud Config本地拉取加上简单认证。

---
+++ spring-cloud-config-sample/spring-cloud-config-server-9003/src/main/java/com/example/study/SpringCloudConfigServer9003.java
@@ -0,0 +1,13 @@
+package com.example.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServer9003 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServer9003.class, args);
+    }
+}
